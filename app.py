79976aa2f27c8ada74b4cfa1b6d72d40c6dd1c2d
fippartners <87595682+fippartners@users.noreply.github.com>
2023-05-28 22:28:12 +0900

    Update app.py by0528IT

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ def communicate():
 
 
 # ユーザーインターフェイスの構築
-st.title("My AI Assistant")
-st.write("ChatGPT APIを使ったチャットボットです。")
+st.title("Dev AiAccistant crtedBy I.T")
+st.write("ChatGPT APIを使ったチャットボットのテストです")
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
